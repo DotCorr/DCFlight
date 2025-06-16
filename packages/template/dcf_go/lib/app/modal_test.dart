9e983ec1f8c2888dbd75670daf02f77993944cad
Tahiru Agbanwa <130235676+squirelboy360@users.noreply.github.com>
2025-06-16 11:42:13 +0200

    move form local setup to public (refactor)

--- packages/template/dcf_go/lib/app/modal_test.dart
+++ packages/template/dcf_go/lib/app/modal_test.dart
@@ -1,9 +1,3 @@
-/*
- * Copyright (c) Dotcorr Studio. and affiliates.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- */
 
 
 import 'package:dcflight/dcflight.dart';
