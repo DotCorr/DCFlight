a4cbd53271cb4961f912cd8f25b18c6d30286e47
Tahiru Agbanwa <130235676+squirelboy360@users.noreply.github.com>
2025-06-24 11:53:30 +0200

    example cleaup

--- packages/template/dcf_go/lib/app/app.dart
+++ packages/template/dcf_go/lib/app/app.dart
@@ -22,276 +22,53 @@ class App extends StatefulComponent {
         DCFSafeAreaView(
           layout: LayoutProps(flex: 1, padding: 8),
           children: [
-           
-        DCFView(
-          layout: LayoutProps(flex: 1, flexDirection: YogaFlexDirection.column),
-          children: [
-            pagestateLocal.state == 0
-                ? Home()
-              
-                : pagestateLocal.state == 1
-                ? GradientTest()
-                : pagestateLocal.state == 2
-                ? ReallyLongList()
-                : pagestateLocal.state == 3
-                ? ModalTest()
-              
-     
-                : pagestateLocal.state == 4
-                ? PortalTest()
-                : DCFView(),
-          ],
-        ),
+            DCFView(
+              layout: LayoutProps(
+                flex: 1,
+                flexDirection: YogaFlexDirection.column,
+              ),
+              children: [
+                pagestateLocal.state == 0
+                    ? Home()
+                    : pagestateLocal.state == 1
+                    ? GradientTest()
+                    : pagestateLocal.state == 2
+                    ? ReallyLongList()
+                    : pagestateLocal.state == 3
+                    ? ModalTest()
+                    : pagestateLocal.state == 4
+                    ? PortalTest()
+                    : DCFView(),
+              ],
+            ),
 
-         DCFDropdown(
+            DCFDropdown(
               layout: LayoutProps(height: 40, marginBottom: 8),
               onValueChange: (v) {
                 pagestateLocal.setState(int.parse(v['value']));
               },
-          onClose: (v) {
-            print("Dropdown closed with value: ${v['value']}");
-          },
-          onOpen: (v) {
-            print("Dropdown opened with value: ${v['value']}");
-          },
-          dropdownProps: DCFDropdownProps(
-            items: [
-              DCFDropdownMenuItem(title: "Home", value: "0"),
-     
-              DCFDropdownMenuItem(title: "Gradient Test", value: "1"),
-              DCFDropdownMenuItem(title: "Really Long List", value: "2"),
-              DCFDropdownMenuItem(title: "Modal & Alert Test", value: "3"),
-       
-              DCFDropdownMenuItem(title: "Portal Test", value: "4"),
-             
-            ],
-            selectedValue: pagestateLocal.state.toString(),
-          ),
-        ),
+              onClose: (v) {
+                print("Dropdown closed with value: ${v['value']}");
+              },
+              onOpen: (v) {
+                print("Dropdown opened with value: ${v['value']}");
+              },
+              dropdownProps: DCFDropdownProps(
+                items: [
+                  DCFDropdownMenuItem(title: "Home", value: "0"),
 
-        DCFScrollView(
-          styleSheet: StyleSheet(
-            backgroundColor: Colors.grey[200]!,
-            borderRadius: 8,
-          ),
-          horizontal: true,
-          showsScrollIndicator: true,
-          layout: LayoutProps(
-            height: 70,
-            padding: 10,
-            paddingHorizontal: 8,
-            flexWrap: YogaWrap.nowrap,
-          ),
-          children: [
-            DCFView(
-              layout: LayoutProps(
-                flexDirection: YogaFlexDirection.row,
-                justifyContent: YogaJustifyContent.spaceAround,
-                alignItems: YogaAlign.center,
-                gap: 8,
-                height: 50,
-                flexWrap: YogaWrap.nowrap,
-                width: "100%",
+                  DCFDropdownMenuItem(title: "Gradient Test", value: "1"),
+                  DCFDropdownMenuItem(title: "Really Long List", value: "2"),
+                  DCFDropdownMenuItem(title: "Modal & Alert Test", value: "3"),
+
+                  DCFDropdownMenuItem(title: "Portal Test", value: "4"),
+                ],
+                selectedValue: pagestateLocal.state.toString(),
               ),
-              children: [
-                DCFGestureDetector(
-                  onTap: (v) {
-                    pagestateLocal.setState(0);
-                  },
-                  layout: LayoutProps(
-                    height: 50,
-                    width: 50,
-                    marginHorizontal: 4,
-                  ),
-                  children: [
-                    DCFView(
-                      layout: LayoutProps(
-                        flex: 1,
-                        justifyContent: YogaJustifyContent.center,
-                        alignItems: YogaAlign.center,
-                      ),
-                      children: [
-                        DCFIcon(
-                          iconProps: DCFIconProps(
-                            name: DCFIcons.house,
-                            color:
-                                pagestateLocal.state == 0
-                                    ? Colors.blue
-                                    : Colors.grey,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
-
-                DCFGestureDetector(
-                  onTap: (v) {
-                    pagestateLocal.setState(1);
-                  },
-                  layout: LayoutProps(
-                    height: 50,
-                    width: 50,
-                    marginHorizontal: 4,
-                  ),
-                  children: [
-                    DCFView(
-                      layout: LayoutProps(
-                        flex: 1,
-                        justifyContent: YogaJustifyContent.center,
-                        alignItems: YogaAlign.center,
-                      ),
-                      children: [
-                        DCFIcon(
-                          iconProps: DCFIconProps(
-                            name: DCFIcons.list,
-                            color:
-                                pagestateLocal.state == 1
-                                    ? Colors.blue
-                                    : Colors.grey,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
-
-                DCFGestureDetector(
-                  onTap: (v) {
-                    pagestateLocal.setState(2);
-                  },
-                  layout: LayoutProps(
-                    height: 50,
-                    width: 50,
-                    marginHorizontal: 4,
-                  ),
-                  children: [
-                    DCFView(
-                      layout: LayoutProps(
-                        flex: 1,
-                        justifyContent: YogaJustifyContent.center,
-                        alignItems: YogaAlign.center,
-                      ),
-                      children: [
-                        DCFIcon(
-                          iconProps: DCFIconProps(
-                            name: DCFIcons.palette,
-                            color:
-                                pagestateLocal.state == 2
-                                    ? Colors.blue
-                                    : Colors.grey,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
-
-                DCFGestureDetector(
-                  onTap: (v) {
-                    pagestateLocal.setState(3);
-                  },
-                  layout: LayoutProps(
-                    height: 50,
-                    width: 50,
-                    marginHorizontal: 4,
-                  ),
-                  children: [
-                    DCFView(
-                      layout: LayoutProps(
-                        flex: 1,
-                        justifyContent: YogaJustifyContent.center,
-                        alignItems: YogaAlign.center,
-                      ),
-                      children: [
-                        DCFIcon(
-                          iconProps: DCFIconProps(
-                            name: DCFIcons.recycle,
-                            color:
-                                pagestateLocal.state == 3
-                                    ? Colors.blue
-                                    : Colors.grey,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
-
-              
-
-              
-
-                DCFGestureDetector(
-                  onTap: (v) {
-                    pagestateLocal.setState(4);
-                  },
-                  layout: LayoutProps(
-                    height: 50,
-                    width: 50,
-                    marginHorizontal: 4,
-                  ),
-                  children: [
-                    DCFView(
-                      layout: LayoutProps(
-                        flex: 1,
-                        justifyContent: YogaJustifyContent.center,
-                        alignItems: YogaAlign.center,
-                      ),
-                      children: [
-                        DCFIcon(
-                          iconProps: DCFIconProps(
-                            name: DCFIcons.grid3x3,
-                            color:
-                                pagestateLocal.state == 4
-                                    ? Colors.blue
-                                    : Colors.grey,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
-
-                DCFGestureDetector(
-                  onTap: (v) {
-                    pagestateLocal.setState(6);
-                  },
-                  layout: LayoutProps(
-                    height: 50,
-                    width: 50,
-                    marginHorizontal: 4,
-                  ),
-                  children: [
-                    DCFView(
-                      layout: LayoutProps(
-                        flex: 1,
-                        justifyContent: YogaJustifyContent.center,
-                        alignItems: YogaAlign.center,
-                      ),
-                      children: [
-                        DCFIcon(
-                          iconProps: DCFIconProps(
-                            name: DCFIcons.shuffle,
-                            color:
-                                pagestateLocal.state == 6
-                                    ? Colors.blue
-                                    : Colors.grey,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
-              ], // Close DCFView children (inside DCFScrollView)
-            ), // Close DCFView (inside DCFScrollView)
-          ], // Close DCFScrollView children
-        ), // Close DCFScrollView
-      ], // Close DCFSafeAreaView children
-        ), // Close DCFSafeAreaView
-        
-      
-      ], // Close main DCFView children
-    ); // Close main DCFView (return statement)
+            ),
+          ],
+        ),
+      ],
+    );
   }
 }
--- packages/template/dcf_go/lib/main.dart
+++ packages/template/dcf_go/lib/main.dart
@@ -1,11 +1,8 @@
-
-
+import 'package:dcf_go/app/app.dart';
 import 'package:dcflight/dcflight.dart';
 
-void main (){
-  DCFlight.start(
-    app: ScreenAPITest()
-  );
+void main() {
+  DCFlight.start(app: ScreenAPITest());
 }
 
 // Temporary test for Screen API
@@ -13,54 +10,22 @@ class ScreenAPITest extends StatefulComponent {
   @override
   DCFComponentNode render() {
     final currentTab = useState<int>(0);
-    
-    return DCFView(
-      layout: LayoutProps(flex: 1),
+
+    return DCFFragment(
       children: [
-        // Define individual screens for tabs
         DCFScreen(
           name: "test_home",
           presentationStyle: DCFPresentationStyle.tab,
-          tabConfig: DCFTabConfig(
-            title: "Home",
-            icon: "house",
-            index: 0,
-          ),
+          tabConfig: DCFTabConfig(title: "Home", icon: "house", index: 0),
           onAppear: (data) => print("✅ Home screen appeared: $data"),
           onActivate: (data) => print("🟢 Home screen activated: $data"),
-          children: [
-            DCFView(
-              layout: LayoutProps(
-                flex: 1,
-                justifyContent: YogaJustifyContent.center,
-                alignItems: YogaAlign.center,
-                padding: 20,
-              ),
-              children: [
-                DCFText(
-                  content: "Home Screen in action ",
-                  textProps: DCFTextProps(
-                    fontSize: 24,
-                    fontWeight: DCFFontWeight.bold,
-                  ),
-                ),
-                DCFText(
-                  content: "Screen API Test - Tab 1",
-                  textProps: DCFTextProps(fontSize: 16),
-                ),
-              ],
-            ),
-          ],
+          children: [App()],
         ),
-        
+
         DCFScreen(
           name: "test_profile",
           presentationStyle: DCFPresentationStyle.tab,
-          tabConfig: DCFTabConfig(
-            title: "Profile",
-            icon: "person",
-            index: 1,
-          ),
+          tabConfig: DCFTabConfig(title: "Profile", icon: "person", index: 1),
           onAppear: (data) => print("✅ Profile screen appeared: $data"),
           onActivate: (data) => print("🟢 Profile screen activated: $data"),
           children: [
@@ -88,15 +53,11 @@ class ScreenAPITest extends StatefulComponent {
             ),
           ],
         ),
-        
+
         DCFScreen(
           name: "test_settings",
           presentationStyle: DCFPresentationStyle.tab,
-          tabConfig: DCFTabConfig(
-            title: "Settings",
-            icon: "gear",
-            index: 2,
-          ),
+          tabConfig: DCFTabConfig(title: "Settings", icon: "gear", index: 2),
           onAppear: (data) => print("✅ Settings screen appeared: $data"),
           onActivate: (data) => print("🟢 Settings screen activated: $data"),
           children: [
@@ -123,7 +84,7 @@ class ScreenAPITest extends StatefulComponent {
             ),
           ],
         ),
-        
+
         // Tab navigator that coordinates the screens
         DCFTabNavigator(
           screens: ["test_home", "test_profile", "test_settings"],
@@ -143,10 +104,7 @@ class ScreenAPITest extends StatefulComponent {
             print("👆 Tab pressed: ${data["selectedIndex"]}");
           },
         ),
-        
       ],
     );
   }
 }
-
-
