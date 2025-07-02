83c0512d37bd6459998e4ae36710d26621638eec
Tahiru Agbanwa <130235676+squirelboy360@users.noreply.github.com>
2025-07-02 12:47:42 +0200

    Revert "Handle hot restart in VDom root creation" +  comments (template)
    This reverts commit eb3041463f5ea1ffba318b91ecb095809e9d2c69.

--- packages/template/dcf_go/lib/main.dart
+++ packages/template/dcf_go/lib/main.dart
@@ -7,8 +7,14 @@ void main() {
 }
 
 class MyApp extends StatefulComponent {
-  // Instantiate registry components once as instance fields.
+  //? Instantiate registry components once as instance fields.
   // This prevents them from being recreated on every render.
+  // Better still use useMemo to create them only once.
+  // These react like hooks are deeply integrated into the component lifecycle.
+  // And they are made such that flutter developers can use them without thinking much about memorizing hooks.
+  // Most of your optimizations are handled by the framework itself and UI modules that follow the framework standards.
+  //
+  // The hooks are as well made intentionally to be as little as possible to avoid unnecessary complexity.
   final _tabRegistry = TabRegistry();
   final _stackRegistry = StackRegistry();
 
