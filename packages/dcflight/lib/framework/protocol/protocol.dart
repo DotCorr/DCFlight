6cf65bcdf7c23c3474d7b99e73d8a823ac18cfb1
Tahiru Agbanwa <130235676+squirelboy360@users.noreply.github.com>
2025-07-05 22:23:04 +0200

    Create protocol.dart

---
+++ packages/dcflight/lib/framework/protocol/protocol.dart
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) Dotcorr Studio. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+
+
+
+
+// Registeration of components happen at the native side now. 
+// This directory files are deprecated and are onlyt used for reference incase i have to reimplement them for ARCH2 (Move old Arch from DCMAUI(Old private framework) to new as new arch).
