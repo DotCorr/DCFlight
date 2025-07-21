04d17a97f2f5a2f4fd652e425fa264392279317b
Tahiru Agbanwa <130235676+squirelboy360@users.noreply.github.com>
2025-07-21 21:43:42 +0200

    Update animation_modal.dart

--- packages/template/dcf_go/lib/features/animation_modal.dart
+++ packages/template/dcf_go/lib/features/animation_modal.dart
@@ -4,15 +4,41 @@ class AnimatedModalScreen extends StatelessComponent {
   
   @override
   DCFComponentNode render() {
-    return DCFView(
+    return DCFScrollView(
       styleSheet: StyleSheet(backgroundColor: Colors.amber),
       layout: LayoutProps(
-        flex: 1,
+       height: "100%",
+        width: "100%",
         padding: 16,
         gap: 2,
         alignContent: YogaAlign.spaceBetween,
         justifyContent: YogaJustifyContent.spaceBetween,
+       
       ),
+
+      children: [
+        DCFText(
+          content: "Animated Modal Screen",
+          textProps: DCFTextProps(
+            fontSize: 24,
+            fontWeight: DCFFontWeight.bold,
+            color: Colors.black,
+          ),
+        ),
+        DCFText(
+          content: "Animation below",
+          textProps: DCFTextProps(fontSize: 16, color: Colors.grey.shade600),
+        ),
+        DCFButton(
+          buttonProps: DCFButtonProps(title: "Dismiss Modal"),
+          layout: LayoutProps(height: 50, width: 200),
+          styleSheet: StyleSheet(backgroundColor: Colors.red, borderRadius: 8),
+          onPress: (v) {
+          
+          
+          },
+        ),
+      ],
     );
   }
 }
