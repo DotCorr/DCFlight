038310ce6356fe2eaf6abeeac659cab0d8ed29b1
Tahiru Agbanwa <130235676+squirelboy360@users.noreply.github.com>
2025-10-11 12:02:22 +0200

    Update main.dart

--- packages/template/dcf_go/lib/main.dart
+++ packages/template/dcf_go/lib/main.dart
@@ -5,51 +5,29 @@ import 'package:dcf_primitives/dcf_primitives.dart';
 import 'dart:math';
 
 void main() async {
-  await DCFlight.go(app: TodoApp());
+  await DCFlight.go(app: MyApp());
 }
 
-class TodoApp extends DCFStatefulComponent {
+class MyApp extends DCFStatefulComponent {
   @override
   DCFComponentNode render() {
-    final todos = useState<List<String>>([]);
-    final input = useState('');
-    
-    return DCFView(
-      layout: DCFLayout(
-        flex: 1,
-        padding: 20,
-        gap: 10,
-        alignItems: YogaAlign.center,
-        justifyContent: YogaJustifyContent.center,
+    return DCFStackNavigationRoot(
+      initialScreen: "home",
+      screenRegistryComponents: StackScreenRegistry(),
+      navigationBarStyle: DCFNavigationBarStyle(
+        backgroundColor: Colors.orange.shade400,
+        titleColor: Colors.white,
+        backButtonColor: Colors.white,
       ),
-      children: [
-        DCFTextInput(
-          value: input.state,
-          onChangeText: (text) {
-            input.setState(text);
-          },
-        ),
-        DCFText(content: todos.toString(), textProps: DCFTextProps(color: Colors.black)),
-        DCFButton(
-          buttonProps: DCFButtonProps(title: 'Add Todo'),
-          onPress: (v) {
-            if (input.state.isNotEmpty) {
-              todos.setState([...todos.state, input.state]);
-              input.setState('');
-            }
-          },
-        ),
-        ...todos.state.map((todo) => 
-          DCFText(
-
-            content: todo,
-            textProps: DCFTextProps(fontSize: 16,color: Colors.black),
-          )
-        ),
-      ],
+      onNavigationChange: (data) {
+        print("🧭 Navigation changed: $data");
+      },
+      onBackPressed: (data) {
+        print("⬅️ Back button pressed: $data");
+      },
     );
   }
-  
+
   @override
   List<Object?> get props => [];
 }
